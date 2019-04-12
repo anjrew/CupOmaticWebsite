ac8a9c584e46c44065aa52a60098902035aa6ebc
earyzhe <earyzhe@gmail.com>
2019-04-12 20:47:26 +0200

    Going well now

--- cupomatic.net/js/script.js
+++ cupomatic.net/js/script.js
@@ -1,27 +1,36 @@
+/* eslint-disable no-trailing-spaces */
 (function () {
 
-    var fadeIn = [
-        'fadeInLeft',
-        'fadeInUp',
-        'fadeInDown',
-        'fadeInRight'
-    ];
+  var fadeIn = [
+    'fadeInLeft',
+    'fadeInUp',
+    'fadeInDown',
+    'fadeInRight'
+  ];
 
-    var fadeInLefts = document.getElementsByclass('fadeInLeft');
-    var fadeInUps = document.getElementsByclass('fadeInUp');
-    var fadeInDowns = document.getElementsByclass('fadeInDown');
-    var fadeInRights = document.getElementsByclass('fadeInRight');
+  var fadeInLefts = document.getElementsByclass('fadeInLeft');
+  var fadeInUps = document.getElementsByclass('fadeInUp');
+  var fadeInDowns = document.getElementsByclass('fadeInDown');
+  var fadeInRights = document.getElementsByclass('fadeInRight');
 
-    function setup(){
-        
-        setUpFades(fadeInLefts, fadeIn[i]);
+  setup()
+
+  function setup() {
+
+    setUpFades(fadeInLefts, fadeIn[i])
+  }
+
+  function setupFades( elements, classToAdd) {
+
+    for (var i = 0; i < elements.length; i++) {
+        elements.classToAdd
     }
+    
+  }
 
-
-    if ($('#element').visible(true)) {
-        // The element is visible, do something
-    } else {
-        // The element is NOT visible, do something else
-    }
-
+  if ($('#element').visible(true)) {
+    // The element is visible, do something
+  } else {
+    // The element is NOT visible, do something else
+  }
 })();
