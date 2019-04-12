e73e39a6224b8de837906b8fed9ea27da74d9cf6
earyzhe <earyzhe@gmail.com>
2019-04-12 16:39:30 +0200

    Altering hero

---
+++ cupomatic.net/js/script.js
@@ -0,0 +1,27 @@
+(function () {
+
+    var fadeIn = [
+        'fadeInLeft',
+        'fadeInUp',
+        'fadeInDown',
+        'fadeInRight'
+    ];
+
+    var fadeInLefts = document.getElementsByclass('fadeInLeft');
+    var fadeInUps = document.getElementsByclass('fadeInUp');
+    var fadeInDowns = document.getElementsByclass('fadeInDown');
+    var fadeInRights = document.getElementsByclass('fadeInRight');
+
+    function setup(){
+        
+        setUpFades(fadeInLefts, fadeIn[i]);
+    }
+
+
+    if ($('#element').visible(true)) {
+        // The element is visible, do something
+    } else {
+        // The element is NOT visible, do something else
+    }
+
+})();
