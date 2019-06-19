5cec76d63002aecf355ad87af5566e93d8ca76c7
earyzhe <earyzhe@gmail.com>
2019-06-19 18:54:03 +0200

    Fade ins are working

--- cupomatic.net/js/script.js
+++ cupomatic.net/js/script.js
@@ -1,36 +1,42 @@
 /* eslint-disable no-trailing-spaces */
 (function () {
 
-  var fadeIn = [
-    'fadeInLeft',
-    'fadeInUp',
-    'fadeInDown',
-    'fadeInRight'
-  ];
+    var fadeIn = [
+        'fadeInLeft',
+        'fadeInUp',
+        'fadeInDown',
+        'fadeInRight'
+    ];
 
-  var fadeInLefts = document.getElementsByclass('fadeInLeft');
-  var fadeInUps = document.getElementsByclass('fadeInUp');
-  var fadeInDowns = document.getElementsByclass('fadeInDown');
-  var fadeInRights = document.getElementsByclass('fadeInRight');
+    var fadeInLefts = document.getElementsByclass('fadeInLeft');
+    var fadeInUps = document.getElementsByclass('fadeInUp');
+    var fadeInDowns = document.getElementsByclass('fadeInDown');
+    var fadeInRights = document.getElementsByclass('fadeInRight');
 
-  setup()
+    setup();
 
-  function setup() {
+    function setup() {
 
-    setUpFades(fadeInLefts, fadeIn[i])
-  }
-
-  function setupFades( elements, classToAdd) {
-
-    for (var i = 0; i < elements.length; i++) {
-        elements.classToAdd
+        setUpFades(fadeInLefts, fadeIn[i]);
     }
-    
-  }
 
-  if ($('#element').visible(true)) {
-    // The element is visible, do something
-  } else {
-    // The element is NOT visible, do something else
-  }
+    function setupFades( elements, classToAdd) {
+
+        for (var i = 0; i < elements.length; i++) {
+            elements.classToAdd;
+        }
+    
+    }
+
+    // Make the things come in from the left when visible
+    for (let index = 0; index < fadeInLefts.length; index++) {
+        const element = fadeInLefts[index];
+        if ($(element).visible(true)) {
+            // The element is visible, do something
+        } else {
+            // The element is NOT visible, do something else
+        }
+    }
+
+  
 })();
